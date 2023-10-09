06869dffab93daf8f5057ffd976d59e73670666d
shavvvv <shavaungreen102@gmail.com>
2023-10-08 21:51:58 -0400

    created variables, inital testing

---
+++ js/app.js
@@ -0,0 +1,59 @@
+// types of bets
+const fiveDollar = 5;
+const tenDollar = 10;
+
+
+//straight up bets
+
+let zero = 0
+let one = 1
+let two = 2
+let three = 3
+let four = 4
+let five = 5
+let six =  6
+let seven = 7
+let eight = 8
+let nine = 9
+let ten = 10
+let eleven = 11
+let twelve = 12
+let thirteen = 13
+let fourteen = 14
+let fifteen = 15
+let sixteen = 16
+let seventeen = 17
+let eighteen = 18
+let nineteen = 19
+let twenty = 20
+let twentyOne = 21
+let twentyTwo = 22
+let twentyThree = 23
+let twentyFour = 24
+let twentyFive = 25
+let twentySix = 26
+let twentySeven = 27
+let twentyEight = 28
+let twentyNine = 29
+let thirty = 30
+let thirtyOne = 31
+let thirtyTwo = 32
+let thirtyThree = 33
+let thirtyFour = 34
+let thirtyFive = 35
+let thirtySix = 36
+
+
+//line bets 
+
+
+let lb0123 = [zero,one,two,three]
+
+
+
+
+let bet=fiveDollar
+
+
+
+
